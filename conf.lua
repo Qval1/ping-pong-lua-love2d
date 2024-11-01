4bebf136709f4e128867cccedc256bd8e04ea312
Qval1 <75909329+Qval1@users.noreply.github.com>
2024-11-01 21:15:58 +0400

    Update conf.lua

--- conf.lua
+++ conf.lua
@@ -1,5 +1,5 @@
 function love.conf(t)
-    t.window.title = "Pag-Pog"
+    t.window.title = "Ping-Pong"
     t.window.width = 800
     t.window.height = 350
     t.window.icon = "icon/game_icon.jpg"
